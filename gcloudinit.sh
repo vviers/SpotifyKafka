93af4687f6a1e1bff940cd09757aef9afa40b46f
VincentViers <vincent.viers@sciencespo.fr>
2019-03-19 10:20:08 +0000

    add gcloud initialisation

---
+++ gcloudinit.sh
@@ -0,0 +1,4 @@
+gcloud dataproc clusters create cluster-spotify-vviers \
+ --subnet default --zone europe-west2-a --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 1.3-deb9 --project st446-vviers \
+ --initialization-actions 'gs://dataproc-initialization-actions/jupyter/jupyter.sh','gs://dataproc-initialization-actions/python/pip-install.sh','gs://dataproc-initialization-actions/zookeeper/zookeeper.sh','gs://dataproc-initialization-actions/kafka/kafka.sh' \
+ --metadata 'PIP_PACKAGES=sklearn nltk pandas graphframes pyspark kafka-python tweepy'
